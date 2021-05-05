0e46b5694958c771f9df0f8a440c63f882479d28
Vafthrudnir <szd0517@gmail.com>
2021-05-05 06:00:50 +0000

    volume setup fixed

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -d --name strava_parser -v save_data:/usr/src/app/save_data -p 80:5000 strava_parser
+docker run -d --name strava_parser -v /$(pwd)/save_data:/usr/src/app/save_data -p 80:5000 strava_parser
