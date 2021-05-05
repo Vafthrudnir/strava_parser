b5c488de05abba6a2320dffa1434a122069a73fa
David Szabo <david_szabo@mentor.com>
2021-05-05 11:53:43 +0200

    accidental host.sh modification reverted

--- host.sh
+++ host.sh
@@ -3,5 +3,4 @@
 export CLIENT_SECRET=`cat client_secret`
 export REFRESH_PASSWORD=`cat refresh_password`
 export FLASK_APP=main.py
-export FLASK_ENV=development
-flask run #--host=0.0.0.0
+flask run --host=0.0.0.0
